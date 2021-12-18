6cc80aa8f012487a93e707cde969f7ca8ed0418e
Dogancan Ulgu <dogancanulgu@gmail.com>
2021-12-19 02:54:46 +0300

    Add partials

--- smartedu_con/pages/views.py
+++ smartedu_con/pages/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
     # return HttpResponse("<h1>INDEX SAYFASI</h1>")
+
+def about(request):
+    return render(request, 'about.html')
